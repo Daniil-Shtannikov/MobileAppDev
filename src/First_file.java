c8fc02bb98edba0d3d528467dc1189629fb1f9ce
User <User@DESKTOP-FUSRB4G>
2025-01-21 18:07:27 +0300

    12 задача

--- src/First_file.java
+++ src/First_file.java
@@ -2,15 +2,16 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class main {
-    static int factorial(int x){
+public class First_file{
+/*    static int factorial(int x){
 
         if (x <= 1){
             return 1;
         }
         return x * factorial(x - 1);
     }
-    public static void main (String[] args) throws java.lang.Exception {
+*/
+    public static void Main (String[] args) throws java.lang.Exception {
 //Задача 1: Четное или нечетное число
         Scanner scanner = new Scanner(System.in);
         /*System.out.print("Input a number: ");
@@ -120,13 +121,33 @@ public class main {
         strBuffer.reverse();
         System.out.println("Перевернутая строка: "+strBuffer.toString());
 
-
-Задача 11: Факториал числа
-Условие: Пользователь вводит целое число N. Программа должна вычислить факториал этого числа (N!).*/
+//Задача 10: Количество цифр в числе
 
         System.out.println("Введите число: ");
-    	int N = scanner.nextInt();
+        int N = scanner.nextInt();
+        int count=0;
+        while(N>0)
+        {
+            count++;
+            N= N/10;
+        }
+        System.out.println(count+" цифр");
 
-        System.out.println("Факториал числа= " + factorial(N));
+//Задача 11: Факториал числа
+
+        System.out.println("Введите число: ");
+        int N = scanner.nextInt();
+        System.out.println("Факториал числа= " +factorial(N));*/
+
+//Задача 12: Сумма цифр числа
+        System.out.println("Введите число: ");
+        int N = scanner.nextInt();
+        int Sum=0;
+        while(N>0)
+        {
+            Sum+= N%10;
+            N = N/10;
+        }
+        System.out.println("Сумма цифр= "+Sum);
     }
 }
