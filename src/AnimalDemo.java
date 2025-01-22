792e2a57b0d19b6f47b25147b42a8aa2f4c8d385
User <User@DESKTOP-FUSRB4G>
2025-01-22 16:56:45 +0300

    Animal

---
+++ src/AnimalDemo.java
@@ -0,0 +1,28 @@
+
+class Dog implements Animal {
+    public void makeSound() {
+        System.out.println("Гав!");
+    }
+}
+
+class Cat implements Animal {
+    public void makeSound() {
+        System.out.println("Мау!");
+    }
+}
+
+class Cow implements Animal {
+    public void makeSound() {
+        System.out.println("Муу!");
+    }
+}
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal[] animals = {new Dog(), new Cat(), new Cow()};
+
+        for (Animal animal : animals) {
+            animal.makeSound();
+        }
+    }
+}
